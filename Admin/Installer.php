ae36c0442a216c0c9f3983f25e0fcd088b727f53
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-11-29 21:57:18 +0100

    Init

---
+++ Admin/Installer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.0
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\PaymentInformation\Admin;
+
+use phpOMS\DataStorage\Database\DatabaseType;
+use phpOMS\DataStorage\Database\Pool;
+use phpOMS\Module\InstallerAbstract;
+
+/**
+ * Accounts receivable install class.
+ *
+ * @category   Modules
+ * @package    Modules\AccountsReceivable
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Installer extends InstallerAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function install(Pool $dbPool, array $info)
+    {
+        parent::install($dbPool, $info);
+
+        switch ($dbPool->get('core')->getType()) {
+            case DatabaseType::MYSQL:
+                $dbPool->get('core')->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'payment_info` (
+                            `payment_info_id` int(11) NOT NULL AUTO_INCREMENT,
+                            `payment_info_account` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`payment_info_id`),
+                            KEY `payment_info_account` (`payment_info_account`)
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
+                )->execute();
+
+                $dbPool->get('core')->con->prepare(
+                    'ALTER TABLE `' . $dbPool->get('core')->prefix . 'payment_info`
+                            ADD CONSTRAINT `' . $dbPool->get('core')->prefix . 'payment_info_ibfk_1` FOREIGN KEY (`payment_info_account`) REFERENCES `' . $dbPool->get('core')->prefix . 'account` (`account_id`);'
+                )->execute();
+                break;
+        }
+    }
+}
