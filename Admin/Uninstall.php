3f8b412fe56bfb362ad7ab1e08a1401301d10449
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-01-16 19:20:55 +0100

    Fixing uninstall and removing old modules

--- Admin/Uninstall.php
+++ Admin/Uninstall.php
@@ -17,6 +17,7 @@ namespace Modules\PaymentInformation\Admin;
 
 
 use phpOMS\DataStorage\Database\Pool;
+use phpOMS\DataStorage\Database\Schema\Builder;
 use phpOMS\Module\UninstallAbstract;
 
 /**
@@ -36,8 +37,16 @@ class Uninstall extends UninstallAbstract
     /**
      * {@inheritdoc}
      */
-    public static function unisntall(Pool $dbPool, array $info)
+    public static function uninstall(Pool $dbPool, array $info)
     {
-        parent::unisntall($dbPool, $info);
+        parent::uninstall($dbPool, $info);
+
+        $query = new Builder($dbPool->get());
+
+        $query->prefix($dbPool->get('core')->getPrefix())->drop(
+            'payment_info'
+        );
+
+        $dbPool->get()->con->prepare($query->toSql())->execute();
     }
 }
