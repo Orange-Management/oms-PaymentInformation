5163a576052e3e20e7d92bd79b1988a08cbb3209
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-09-23 13:58:09 +0200

    Use default database connection

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -38,10 +38,10 @@ class Installer extends InstallerAbstract
     {
         parent::install(__DIR__ . '/..', $dbPool, $info);
 
-        switch ($dbPool->get('core')->getType()) {
+        switch ($dbPool->get()->getType()) {
             case DatabaseType::MYSQL:
-                $dbPool->get('core')->con->prepare(
-                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get('core')->prefix . 'payment_info` (
+                $dbPool->get()->con->prepare(
+                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get()->prefix . 'payment_info` (
                             `payment_info_id` int(11) NOT NULL AUTO_INCREMENT,
                             `payment_info_account` int(11) DEFAULT NULL,
                             PRIMARY KEY (`payment_info_id`),
@@ -49,9 +49,9 @@ class Installer extends InstallerAbstract
                         )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
                 )->execute();
 
-                $dbPool->get('core')->con->prepare(
-                    'ALTER TABLE `' . $dbPool->get('core')->prefix . 'payment_info`
-                            ADD CONSTRAINT `' . $dbPool->get('core')->prefix . 'payment_info_ibfk_1` FOREIGN KEY (`payment_info_account`) REFERENCES `' . $dbPool->get('core')->prefix . 'account` (`account_id`);'
+                $dbPool->get()->con->prepare(
+                    'ALTER TABLE `' . $dbPool->get()->prefix . 'payment_info`
+                            ADD CONSTRAINT `' . $dbPool->get()->prefix . 'payment_info_ibfk_1` FOREIGN KEY (`payment_info_account`) REFERENCES `' . $dbPool->get()->prefix . 'account` (`account_id`);'
                 )->execute();
                 break;
         }
