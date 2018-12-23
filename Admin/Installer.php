ca3b1af4203cb6c593a590c7afab0468210fce3b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-23 20:30:11 +0100

    Move schema to json

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -29,30 +29,4 @@ use phpOMS\Module\InstallerAbstract;
  */
 class Installer extends InstallerAbstract
 {
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function install(DatabasePool $dbPool, InfoManager $info) : void
-    {
-        parent::install($dbPool, $info);
-
-        switch ($dbPool->get()->getType()) {
-            case DatabaseType::MYSQL:
-                $dbPool->get()->con->prepare(
-                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get()->prefix . 'payment_info` (
-                            `payment_info_id` int(11) NOT NULL AUTO_INCREMENT,
-                            `payment_info_account` int(11) DEFAULT NULL,
-                            PRIMARY KEY (`payment_info_id`),
-                            KEY `payment_info_account` (`payment_info_account`)
-                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
-                )->execute();
-
-                $dbPool->get()->con->prepare(
-                    'ALTER TABLE `' . $dbPool->get()->prefix . 'payment_info`
-                            ADD CONSTRAINT `' . $dbPool->get()->prefix . 'payment_info_ibfk_1` FOREIGN KEY (`payment_info_account`) REFERENCES `' . $dbPool->get()->prefix . 'account` (`account_id`);'
-                )->execute();
-                break;
-        }
-    }
 }
