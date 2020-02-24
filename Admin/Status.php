8815b7b58550791ae5af143449e0a4f6a43a7672
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-24 22:57:26 +0100

    make module status handling final

--- Admin/Status.php
+++ Admin/Status.php
@@ -17,13 +17,13 @@ namespace Modules\PaymentInformation\Admin;
 use phpOMS\Module\StatusAbstract;
 
 /**
- * Navigation class.
+ * Status class.
  *
  * @package Modules\PaymentInformation\Admin
  * @license OMS License 1.0
  * @link    https://orange-management.org
  * @since   1.0.0
  */
-class Status extends StatusAbstract
+final class Status extends StatusAbstract
 {
 }
